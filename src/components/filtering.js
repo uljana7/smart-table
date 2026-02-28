c25cab3333a2321cfcbb6c7b86460e805ca0cb31
Uliana Semenova <uljana736@gmail.com>
2026-02-28 16:03:49 +0300

    запрограммирована логика работы таблицы

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -4,11 +4,37 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach(elementName => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName])
+                .map(name => {
+                    const option = document.createElement('option');
+                    option.value = name;
+                    option.textContent = name;
+                    return option;
+                })
+        );
+    });
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            // Находим кнопку, которая вызвала действие (action), ищем её родителя
+            const clearBtn = action;
+            const parent = clearBtn.parentElement;
+            if (parent) {
+                // Находим input с атрибутом data-field аналогично кнопке
+                const input = parent.querySelector('input');
+                if (input) {
+                    input.value = '';
+                    const field = clearBtn.dataset.field; // поле фильтрации
+                    state[field] = '';                     // очищаем фильтр в состоянии
+                }
+            }
+        }
+        // @todo: #4.3 — настроить компаратор
+        const compare = createComparison(defaultRules);
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -2,10 +2,21 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        { skipEmptyTargetValues: true }, // правило, игнорировать пустые значения
+        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
+    );
 
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
+        const searchValue = state[searchField];
+
+        // Если поисковое поле пустое или не задано - возвращаем исходные данные
+        if (!searchValue || !searchValue.trim()) {
         return data;
+        }
+
+        // Иначе — фильтруем данные по поисковому запросу
+        return data.filter(row => compare(row, state));
+    
     }
 }
